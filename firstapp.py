4bb9199ccc4e8e9d67c931c76c50d5e70367bad6
ducpham <babylearnmaths@gmail.com>
2019-12-02 16:13:35 +0700

    first commit

---
+++ firstapp.py
@@ -0,0 +1,7 @@
+from pyspark import SparkContext
+logFile = "file:///home/hduser/spark-2.4.4-bin-hadoop2.6/README.md"  
+sc = SparkContext("local", "first app")
+logData = sc.textFile(logFile).cache()
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
